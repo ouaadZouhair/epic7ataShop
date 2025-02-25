72094f3902060d2a792100b0b9c265982d847a56
ouaadZouhair <ouaadzouhair11@gmail.com>
2025-02-25 13:53:10 +0000

    Applied .gitignore and removed unwanted files

--- eco-frontend/src/pages/Contact.jsx
+++
@@ -1,6 +0,0 @@
-function Contact() {
-    return <h1>Contact Us</h1>;
-  }
-  
-  export default Contact;
-  
--- eco-frontend/src/pages/Print.jsx
+++
@@ -1,6 +0,0 @@
-function Print() {
-    return <h1>Print </h1>;
-  }
-  
-  export default Print;
-  
